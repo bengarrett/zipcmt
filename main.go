b5fe79a34213d450ec2306d7c34c706b7c6ad6e8
Ben Garrett <code.by.ben@gmail.com>
2021-05-09 13:35:29 +1000

    Created zipcmmt package.

--- main.go
+++ main.go
@@ -3,33 +3,15 @@
 package main
 
 import (
-	"archive/zip"
-	"crypto/sha256"
-	"errors"
 	"flag"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 	"text/tabwriter"
 
-	"github.com/bengarrett/retrotxtgo/lib/convert"
+	"github.com/bengarrett/zipcmt/zipcmmt"
 )
-
-type config struct {
-	exportdir  string
-	exportfile bool
-	nodupes    bool
-	overwrite  bool
-	raw        bool
-	recursive  bool
-	print      bool
-	quiet      bool
-	zips       int
-	cmmts      int
-}
 
 var (
 	version = "0.0.0"
@@ -38,123 +20,115 @@ var (
 )
 
 func main() {
-	var c config
-	flag.BoolVar(&c.print, "print", false, "print the comments to the terminal")
-	flag.BoolVar(&c.raw, "raw", false, "use the original comment text encoding instead of Unicode")
-	flag.BoolVar(&c.exportfile, "export", false, "save the comment to a textfile stored alongside the archive (use at your own risk)")
-	flag.StringVar(&c.exportdir, "exportdir", "", "save the comment to a textfile stored in this directory")
-	flag.BoolVar(&c.recursive, "recursive", false, "recursively walk through all subdirectories while scanning for zip archives")
-	flag.BoolVar(&c.overwrite, "overwrite", false, "overwrite any previously exported comment textfiles")
-	flag.BoolVar(&c.quiet, "quiet", false, "suppress zipcmt feedback except for errors")
-	flag.BoolVar(&c.nodupes, "nodupes", false, "no duplicate comments, only show unique finds")
+	var c zipcmmt.Config
+	var recursive bool
+	flag.BoolVar(&c.Print, "print", false, "print the comments to the terminal")
+	flag.BoolVar(&c.Raw, "raw", false, "use the original comment text encoding instead of Unicode")
+	flag.BoolVar(&c.ExportFile, "export", false, "save the comment to a textfile stored alongside the archive (use at your own risk)")
+	flag.StringVar(&c.ExportDir, "exportdir", "", "save the comment to a textfile stored in this directory")
+	flag.BoolVar(&recursive, "recursive", false, "recursively walk through all subdirectories while scanning for zip archives")
+	flag.BoolVar(&c.Overwrite, "overwrite", false, "overwrite any previously exported comment textfiles")
+	flag.BoolVar(&c.Quiet, "quiet", false, "suppress zipcmt feedback except for errors")
+	flag.BoolVar(&c.NoDupes, "nodupes", false, "no duplicate comments, only show unique finds")
 	ver := flag.Bool("version", false, "version and information for this program")
+	d := flag.String("d", "", "alias for exportdir")
+	e := flag.Bool("e", false, "alias for export")
+	n := flag.Bool("n", false, "alias for nodupes")
+	o := flag.Bool("o", false, "alias for overwrite")
+	q := flag.Bool("q", false, "alias for quiet")
 	r := flag.Bool("r", false, "alias for recursive")
 	u := flag.Bool("p", false, "alias for print")
-	e := flag.Bool("e", false, "alias for export")
-	d := flag.String("d", "", "alias for exportdir")
-	o := flag.Bool("o", false, "alias for overwrite")
 	v := flag.Bool("v", false, "alias for version")
-	q := flag.Bool("q", false, "alias for quiet")
-	n := flag.Bool("n", false, "alias for nodupes")
-
 	flag.Usage = func() {
 		help()
 	}
 	flag.Parse()
-
-	// help convience for when a help flag is passed as an argument
-	for _, arg := range flag.Args() {
-		arg = strings.ToLower(arg)
-		if arg == "-h" || arg == "-help" || arg == "--help" {
-			flag.Usage()
-			return
-		}
-		if arg == "-v" || arg == "-version" || arg == "--version" {
-			info()
-			return
-		}
-	}
-	// print help if no arguments are given
-	if len(flag.Args()) == 0 {
-		flag.Usage()
-		return
-	}
-
-	// version information
-	if *ver || *v {
-		info()
-		return
-	}
+	flags(*ver, *v)
 	// parse aliases
 	if *r {
-		c.recursive = true
+		recursive = true
 	}
 	if *u {
-		c.print = true
+		c.Print = true
 	}
 	if *e {
-		c.exportfile = true
+		c.ExportFile = true
 	}
 	if *d != "" {
-		c.exportdir = *d
+		c.ExportDir = *d
 	}
 	if *o {
-		c.overwrite = true
+		c.Overwrite = true
 	}
 	if *q {
-		c.quiet = true
+		c.Quiet = true
 	}
 	if *n {
-		c.nodupes = true
+		c.NoDupes = true
 	}
-	// export to directory sanity check
-	if c.exportdir != "" {
-		c.exportdir = filepath.Clean(c.exportdir)
-		p := strings.Split(c.exportdir, string(filepath.Separator))
-		if p[0] == "~" {
-			hd, err := os.UserHomeDir()
-			if err != nil {
-				log.Fatalln(err)
-			}
-			c.exportdir = strings.Replace(c.exportdir, "~", hd, 1)
-		}
-		s, err := os.Stat(c.exportdir)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		if !s.IsDir() {
-			log.Fatalln(os.ErrInvalid)
-		}
+	// sanitize the export directory
+	if err := c.Clean(); err != nil {
+		log.Fatalln(err)
 	}
 	// recursive directory scan
-	if c.recursive {
+	if recursive {
 		for _, root := range flag.Args() {
-			if err := c.scans(root); err != nil {
+			if err := c.Scans(root); err != nil {
 				log.Println(err)
 			}
-			c.status()
+			c.Status()
 		}
 		return
 	}
 	// default flat directory scan
 	for _, root := range flag.Args() {
-		if err := c.scan(root); err != nil {
+		if err := c.Scan(root); err != nil {
 			log.Println(err)
 		}
 	}
-	c.status()
+	c.Status()
 }
 
+func flags(ver, v bool) {
+	// convience for when a help or version flag is passed as an argument
+	for _, arg := range flag.Args() {
+		switch strings.ToLower(arg) {
+		case "-h", "-help", "--help":
+			flag.Usage()
+			os.Exit(0)
+		case "-v", "-version", "--version":
+			info()
+			os.Exit(0)
+		}
+	}
+	// print help if no arguments are given
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+		os.Exit(0)
+	}
+	// print version information
+	if ver || v {
+		info()
+		os.Exit(0)
+	}
+}
+
+// Help, usage and examples.
 func help() {
 	var f *flag.Flag
+	const ps = string(os.PathSeparator)
 	fmt.Fprintln(os.Stderr, "Usage:")
 	fmt.Fprintln(os.Stderr, "    zipcmt [options] [directories]")
 	fmt.Fprintln(os.Stderr, "")
 	fmt.Fprintln(os.Stderr, "Examples:")
-	fmt.Fprintln(os.Stderr, "    zipcmt --print --nodupes .\t\t# scan the working directory and only show unique comments")
-	fmt.Fprintln(os.Stderr, "    zipcmt --export ~/Downloads\t\t# scan the download directory and save all comments")
-	fmt.Fprintln(os.Stderr, "    zipcmt -r -d=~/text ~/Downloads\t# recursively scan the download directory and save all comments to a directory")
-	fmt.Fprintln(os.Stderr, "    zipcmt -n -p -q -r / | less\t\t# scan the whole system and view unique comments in a page reader")
+	fmt.Fprint(os.Stderr, "    zipcmt --print --nodupes .\t\t")
+	fmt.Fprintln(os.Stderr, "# scan the working directory and only show unique comments")
+	fmt.Fprintf(os.Stderr, "    zipcmt --export ~%sDownloads\t\t", ps)
+	fmt.Fprintln(os.Stderr, "# scan the download directory and save all comments")
+	fmt.Fprintf(os.Stderr, "    zipcmt -r -d=~%stext ~%sDownloads\t", ps, ps)
+	fmt.Fprintln(os.Stderr, "# recursively scan the download directory and save all comments to a directory")
+	fmt.Fprintf(os.Stderr, "    zipcmt -n -p -q -r %s | less\t\t", ps)
+	fmt.Fprintln(os.Stderr, "# scan the whole system and view unique comments in a page reader")
 	fmt.Fprintln(os.Stderr, "")
 	fmt.Fprintln(os.Stderr, "Options:")
 	w := tabwriter.NewWriter(os.Stderr, 0, 0, 4, ' ', 0)
@@ -184,14 +158,6 @@ func help() {
 	w.Flush()
 }
 
-func self() (string, error) {
-	exe, err := os.Executable()
-	if err != nil {
-		return "", fmt.Errorf("self error: %w", err)
-	}
-	return exe, nil
-}
-
 // Info prints out the program information and version.
 func info() {
 	const copyright = "\u00A9"
@@ -206,189 +172,10 @@ func info() {
 	fmt.Printf("path:  %s\n", exe)
 }
 
-func (c *config) scan(root string) error {
-	hashes := map[[32]byte]bool{}
-	files, err := os.ReadDir(root)
+func self() (string, error) {
+	exe, err := os.Executable()
 	if err != nil {
-		return err
+		return "", fmt.Errorf("self error: %w", err)
 	}
-	for _, file := range files {
-		path := filepath.Join(root, file.Name())
-		if !valid(path) {
-			continue
-		}
-		c.zips = c.zips + 1
-		cmmt, err := c.read(path)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if len(cmmt) == 0 {
-			continue
-		}
-		if c.nodupes {
-			hash := sha256.Sum256([]byte(cmmt))
-			if hashes[hash] {
-				continue
-			}
-			hashes[hash] = true
-		}
-		c.cmmts = c.cmmts + 1
-		c.separator(path)
-		if c.print {
-			if err := c.stdout(cmmt); err != nil {
-				fmt.Println(err)
-			}
-		}
-		if c.exportfile {
-			go save(path, cmmt, c.overwrite)
-		}
-		if c.exportdir != "" {
-			go save(filepath.Join(c.exportdir, file.Name()), cmmt, c.overwrite)
-		}
-	}
-	return nil
-}
-
-func (c *config) scans(root string) error {
-	hashes := map[[32]byte]bool{}
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			if errors.Is(err, fs.ErrPermission) {
-				return nil
-			}
-			return err
-		}
-		if !valid(d.Name()) {
-			return nil
-		}
-		c.zips = c.zips + 1
-		cmmt, err := c.read(path)
-		if err != nil {
-			fmt.Println(err)
-			return nil
-		}
-		if len(cmmt) == 0 {
-			return nil
-		}
-		if c.nodupes {
-			hash := sha256.Sum256([]byte(cmmt))
-			if hashes[hash] {
-				return nil
-			}
-			hashes[hash] = true
-		}
-		c.cmmts = c.cmmts + 1
-		c.separator(path)
-		if c.print {
-			if err := c.stdout(cmmt); err != nil {
-				fmt.Println(err)
-				return nil
-			}
-		}
-		if c.exportfile {
-			fmt.Println(path)
-			save(path, cmmt, c.overwrite)
-		}
-		return err
-	})
-	return err
-}
-
-func valid(name string) bool {
-	const zip = ".zip"
-	switch filepath.Ext(strings.ToLower(name)) {
-	case zip:
-		return true
-	}
-	return false
-}
-
-func (c config) read(name string) (cmmt string, err error) {
-	r, err := zip.OpenReader(name)
-	if err != nil {
-		return "", nil
-	}
-	defer r.Close()
-	if cmmt := r.Comment; cmmt != "" {
-		if strings.TrimSpace(cmmt) == "" {
-			return "", nil
-		}
-		if !c.raw {
-			b, err := convert.D437(cmmt)
-			if err != nil {
-				return "", err
-			}
-			cmmt = string(b)
-		}
-		return cmmt, nil
-	}
-	return "", nil
-}
-
-func save(path, cmmt string, ow bool) {
-	if cmmt == "" {
-		return
-	}
-	name := strings.TrimSuffix(path, filepath.Ext(path)) + "-zipcomment.txt"
-	if !ow {
-		if s, err := os.Stat(name); err == nil {
-			fmt.Printf("export skipped, file already exists: %s (%dB)\n", name, s.Size())
-			return
-		}
-	}
-	f, err := os.Create(name)
-	if err != nil {
-		log.Println(err)
-	}
-	defer f.Close()
-	if i, err := f.Write([]byte(cmmt)); err != nil {
-		log.Println(err)
-	} else if i == 0 {
-		if err1 := os.Remove(name); err != nil {
-			log.Println(err1)
-		}
-	}
-}
-
-func (c config) stdout(cmmt string) error {
-	const resetCmd = "\033[0m"
-	fmt.Printf("%s%s\n", cmmt, resetCmd)
-	return nil
-}
-
-func (c config) separator(name string) {
-	if !c.print || c.quiet {
-		return
-	}
-	const file_id = 45
-	const pointer = " \u2500\u2500 "
-	if h, err := os.UserHomeDir(); err == nil {
-		if len(name) > len(h) && name[0:len(h)] == h {
-			name = strings.Replace(name, h, "~", 1)
-		}
-	}
-	l := len(pointer) + len(name)
-	if l >= file_id {
-		fmt.Printf("%s%s\n", pointer, name)
-		return
-	}
-	fmt.Printf("\n%s%s %s\u2510\n", pointer, name, strings.Repeat("\u2500", file_id-l))
-}
-
-func (c config) status() {
-	if c.quiet {
-		return
-	}
-	a, cm, unq := "archive", "comment", ""
-	if c.zips != 1 {
-		a += "s"
-	}
-	if c.cmmts != 1 {
-		cm += "s"
-	}
-	if c.nodupes {
-		unq = "unique "
-	}
-	fmt.Printf("Scanned %d zip %s and found %d %s%s\n", c.zips, a, c.cmmts, unq, cm)
+	return exe, nil
 }
---
+++ zipcmmt/zipcmmt.go
@@ -0,0 +1,241 @@
+// Package zipcmmmt is a viewer and an extractor of zip archive comments
+package zipcmmt
+
+import (
+	"archive/zip"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bengarrett/retrotxtgo/lib/convert"
+)
+
+type Config struct {
+	ExportDir  string
+	ExportFile bool
+	NoDupes    bool
+	Overwrite  bool
+	Raw        bool
+	Print      bool
+	Quiet      bool
+	zips       int
+	cmmts      int
+}
+
+const filename = "-zipcomment.txt"
+
+var ErrExpFile = errors.New("export directory is a file")
+
+// Clean the syntax of the target export directory path.
+func (c *Config) Clean() error {
+	if c.ExportDir != "" {
+		c.ExportDir = filepath.Clean(c.ExportDir)
+		p := strings.Split(c.ExportDir, string(filepath.Separator))
+		if p[0] == "~" {
+			hd, err := os.UserHomeDir()
+			if err != nil {
+				return err
+			}
+			c.ExportDir = strings.Replace(c.ExportDir, "~", hd, 1)
+		}
+		s, err := os.Stat(c.ExportDir)
+		if err != nil {
+			return err
+		}
+		if !s.IsDir() {
+			return ErrExpFile
+		}
+	}
+	return nil
+}
+
+func (c Config) Read(name string) (cmmt string, err error) {
+	r, err := zip.OpenReader(name)
+	if err != nil {
+		return "", nil
+	}
+	defer r.Close()
+	if cmmt := r.Comment; cmmt != "" {
+		if strings.TrimSpace(cmmt) == "" {
+			return "", nil
+		}
+		if !c.Raw {
+			b, err := convert.D437(cmmt)
+			if err != nil {
+				return "", err
+			}
+			cmmt = string(b)
+		}
+		return cmmt, nil
+	}
+	return "", nil
+}
+
+func (c *Config) Scan(root string) error {
+	hashes := map[[32]byte]bool{}
+	files, err := os.ReadDir(root)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		path := filepath.Join(root, file.Name())
+		if !valid(path) {
+			continue
+		}
+		c.zips++
+		cmmt, err := c.Read(path)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if cmmt == "" {
+			continue
+		}
+		if c.NoDupes {
+			hash := sha256.Sum256([]byte(cmmt))
+			if hashes[hash] {
+				continue
+			}
+			hashes[hash] = true
+		}
+		c.cmmts++
+		c.Separator(path)
+		if c.Print {
+			if err := stdout(cmmt); err != nil {
+				fmt.Println(err)
+			}
+		}
+		if c.ExportFile {
+			go save(path, cmmt, c.Overwrite)
+		}
+		if c.ExportDir != "" {
+			go save(filepath.Join(c.ExportDir, file.Name()), cmmt, c.Overwrite)
+		}
+	}
+	return nil
+}
+
+func (c *Config) Scans(root string) error {
+	hashes := map[[32]byte]bool{}
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			if errors.Is(err, fs.ErrPermission) {
+				return nil
+			}
+			return err
+		}
+		if !valid(d.Name()) {
+			return nil
+		}
+		c.zips++
+		cmmt, err := c.Read(path)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		if cmmt == "" {
+			return nil
+		}
+		if c.NoDupes {
+			hash := sha256.Sum256([]byte(cmmt))
+			if hashes[hash] {
+				return nil
+			}
+			hashes[hash] = true
+		}
+		c.cmmts++
+		c.Separator(path)
+		if c.Print {
+			if err1 := stdout(cmmt); err != nil {
+				fmt.Println(err1)
+				return nil
+			}
+		}
+		if c.ExportFile {
+			fmt.Println(path)
+			save(path, cmmt, c.Overwrite)
+		}
+		return err
+	})
+	return err
+}
+
+func (c Config) Separator(name string) {
+	if !c.Print || c.Quiet {
+		return
+	}
+	const fileID = 45
+	const pointer = " \u2500\u2500 "
+	if h, err := os.UserHomeDir(); err == nil {
+		if len(name) > len(h) && name[0:len(h)] == h {
+			name = strings.Replace(name, h, "~", 1)
+		}
+	}
+	l := len(pointer) + len(name)
+	if l >= fileID {
+		fmt.Printf("%s%s\n", pointer, name)
+		return
+	}
+	fmt.Printf("\n%s%s %s\u2510\n", pointer, name, strings.Repeat("\u2500", fileID-l))
+}
+
+func (c Config) Status() {
+	if c.Quiet {
+		return
+	}
+	a, cm, unq := "archive", "comment", ""
+	if c.zips != 1 {
+		a += "s"
+	}
+	if c.cmmts != 1 {
+		cm += "s"
+	}
+	if c.NoDupes {
+		unq = "unique "
+	}
+	if c.Print {
+		fmt.Println()
+	}
+	fmt.Printf("Scanned %d zip %s and found %d %s%s\n", c.zips, a, c.cmmts, unq, cm)
+}
+
+func save(path, cmmt string, ow bool) {
+	if cmmt == "" {
+		return
+	}
+	name := strings.TrimSuffix(path, filepath.Ext(path)) + filename
+	if !ow {
+		if s, err := os.Stat(name); err == nil {
+			fmt.Printf("export skipped, file already exists: %s (%dB)\n", name, s.Size())
+			return
+		}
+	}
+	f, err := os.Create(name)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if i, err := f.Write([]byte(cmmt)); err != nil {
+		log.Println(err)
+	} else if i == 0 {
+		if err1 := os.Remove(name); err != nil {
+			log.Println(err1)
+		}
+	}
+}
+
+func stdout(cmmt string) error {
+	const resetCmd = "\033[0m"
+	fmt.Printf("%s%s\n", cmmt, resetCmd)
+	return nil
+}
+
+func valid(name string) bool {
+	const z = ".zip"
+	return filepath.Ext(strings.ToLower(name)) == z
+}
