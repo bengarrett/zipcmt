036245c3fe4fc1c448016c4664e197039b796693
Ben Garrett <code.by.ben@gmail.com>
2021-05-12 08:44:29 +1000

    Expanded examples.

--- zipcmt/example_test.go
+++ zipcmt/example_test.go
@@ -7,6 +7,17 @@ import (
 	"log"
 )
 
+func ExampleConfig_Clean() {
+	c := Config{
+		ExportDir: "..//test///.",
+	}
+	if err := c.Clean(); err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Print(c.ExportDir)
+	// Output: ../test
+}
+
 func ExampleConfig_Read() {
 	c := Config{
 		Raw: false,
@@ -32,4 +43,37 @@ func ExampleConfig_Scan() {
 	// Output:
 	//This is an example test comment for zipcmmt.[0m
 	//
+}
+
+func ExampleConfig_Walk() {
+	c := Config{
+		Print: true,
+	}
+	if err := c.Walk("../test"); err != nil {
+		log.Panicln(err)
+	}
+	// Output:
+	// ── ../test/subdir/test-with-comment.zip ─┐
+	//    This is an example test comment for zipcmmt.[0m
+	//
+	//  ── ../test/test-with-comment.zip ────────┐
+	//    This is an example test comment for zipcmmt.[0m
+}
+
+func ExampleConfig_Status() {
+	c := Config{}
+	if err := c.Walk("../test"); err != nil {
+		log.Panicln(err)
+	}
+	fmt.Print(c.Status())
+
+	c = Config{
+		NoDupes: true,
+	}
+	if err := c.Walk("../test"); err != nil {
+		log.Panicln(err)
+	}
+	fmt.Print(c.Status())
+	// Output: Scanned 4 zip archives and found 2 comments
+	// Scanned 4 zip archives and found 1 unique comment
 }
